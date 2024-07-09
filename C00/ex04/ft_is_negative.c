a97d5e7bf01559ccda4bc460acd8300f4a602810
Imane Zemmaoui <imzemmao@e1r7p6.1337.ma>
2024-07-09 12:23:50 +0100

    Initial commit

---
+++ C00/ex04/ft_is_negative.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imzemmao <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/27 11:59:59 by imzemmao          #+#    #+#             */
+/*   Updated: 2024/06/29 10:34:16 by imzemmao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_is_negative(int n)
+{
+	char	negative;
+	char	positive;
+
+	negative = 'N';
+	positive = 'P';
+	if (n < 0)
+	{
+		write(1, &negative, 1);
+	}
+	else
+	{
+		write(1, &positive, 1);
+	}
+}
